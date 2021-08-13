d5729400162152a93423a7a4bb7e3e36e07a013c
Norman Bernard <nksb414@gmail.com>
2021-08-13 17:30:56 +0800

    updated typedefs and data seed

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,28 +5,29 @@ const typeDefs = gql`
     _id: ID
     name: String
     email: String
-    # There is now a field to store the user's password
     password: String
+    age: Number
+    weight: Number
+    gender: String
   }
 
-  # Set up an Auth type to handle returning data from a profile creating or user login
+  type Drink {
+    _id: ID
+    drink: String
+    Potency: Number
+    Volume: Number
+  }
+
   type Auth {
     token: ID!
     user: User
   }
 
-  type Query {
-    users: [User]!
-    user(userId: ID!): User
-  }
-
   type Mutation {
-    # Set up mutations to handle creating a profile or logging into a profile and return Auth type
-    addUser(name: String!, email: String!, password: String!): Auth
+    addUser(name: String!, email: String!, password: String!, age: Number!, weight: Number!, gender: String): Auth
     login(email: String!, password: String!): Auth
-
-    removeProfile(profileId: ID!): Profile
     
+        
   }
 `;
 
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,15 +1,19 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, Drinks } = require('../models');
 const userSeeds = require('./userSeeds.json');
+const drinkSeeds = require('./drinkSeeds.json');
 
 db.once('open', async () => {
   try {
     await User.deleteMany({});
+    await Drinks.deleteMany({});
     await User.create(userSeeds);
+    await Drinks.create(drinkSeeds);
 
-    console.log('all done!');
+    console.log('done seeding data!');
     process.exit(0);
   } catch (err) {
     throw err;
   }
 });
+
